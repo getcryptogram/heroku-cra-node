3344ab0da156d4f58549d820ab0ce60068f6b4e6
Michael Liou <mliou@Michaels-Air.attlocal.net>
2021-05-18 11:27:42 -0500

    final str

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -85,9 +85,9 @@ const App = (props) => {
       drawingNotes: drawingNotes,
     };
     console.log("What is preparedData ", preparedData);
-    preparedData.map((img) => img).join("\n");
+    const finalStr = preparedData.join("\n");
     // const imageString = preparedData.split(",").join("\n");
-    console.log("preparedData is ", preparedData);
+    console.log("finalString is ", finalStr);
     const imageString = "string";
     // console.log("imageString is ", imageString);
     const sendStr = `Order Title: ${orderInfo}
