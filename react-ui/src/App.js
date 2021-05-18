d49d2ec345f0e7fbd01bdd55df8be51cca62963f
Michael Liou <mliou@Michaels-Air.attlocal.net>
2021-05-18 07:50:09 -0500

    adding file

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -20,6 +20,7 @@ const App = (props) => {
   const [drawingNotes, setNotes] = useState("");
   const [orderInfo, setOrderInfo] = useState({});
   const [orderInfoSet, toggleOrderInfo] = useState(false);
+  console.log("correctly print access stuff ", config);
 
   React.useEffect(() => {
     window.addEventListener("message", (event) => {
